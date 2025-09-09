31cb2613f44f25249fdf95df48fcc16e1dcd18ee
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-09 19:23:32 +1000

    [util] String log method

--- program/Util.cs
+++ program/Util.cs
@@ -3,15 +3,20 @@ namespace Connect4
 {
 	public class Util
 	{
-        internal static void LogMatrix(List<List<string>> m_)
-        {
-            string s_ = "";
-            for (int i = 0; i < m_.Count; i++)
-            {
-                s_ += string.Join(",", m_[i].ToArray()) + "\n";
-            }
-            s_ += "\n";
-            File.AppendAllText("matrix.log", s_);
+		internal static void LogMatrix(List<List<string>> m_)
+		{
+			string s_ = "";
+			for (int i = 0; i < m_.Count; i++)
+			{
+				s_ += string.Join(",", m_[i].ToArray()) + "\n";
+			}
+			s_ += "\n";
+			File.AppendAllText("matrix.log", s_);
+		}
+		
+		internal static void LogString(string s_)
+		{
+			File.AppendAllText("matrix.log", s_+"\n");
 		}
 	}
 }
