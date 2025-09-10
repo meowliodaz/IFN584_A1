8e86627953a304b93e0c9ab3b3c68dc94da1f617
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-10 13:31:22 +1000

    [grid] Update "update disc count" logic

--- program/Grid.cs
+++ program/Grid.cs
@@ -179,8 +179,8 @@ namespace Connect4
 						if (Matrix[playedCol][j] == "#") disc2 += 1;
 					}
 					Matrix[playedCol - 1] = newColBore1;
-					P1.Discs = new Dictionary<string,int>(){{"o", P1.Discs["o"] + disc1}};
-					P2.Discs = new Dictionary<string,int>(){{"o", P2.Discs["o"] + disc1}};
+					P1.Discs = new Dictionary<string,int>(){{"o", disc1}};
+					P2.Discs = new Dictionary<string,int>(){{"o", disc2}};
 					DisplayGrid(test);
 
 					// Convert to ordinary
