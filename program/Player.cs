690b7b129e7923b4b1dc2c1b674687c26e221847
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-10 13:28:40 +1000

    [player] Update disc count

--- program/Player.cs
+++ program/Player.cs
@@ -2,6 +2,28 @@ namespace Connect4
 {
 	public class Player
 	{
-		
+		// Fields
+		protected Dictionary<string, int> discs = new Dictionary<string, int>
+		{
+			{"o", 0},
+			{"b", 2},
+			{"m", 2}
+		};
+
+
+		// Properties
+		public Dictionary<string, int> Discs
+		{
+			get
+			{
+				return discs;
+			}
+			set
+			{
+				discs["o"] = value["o"];
+				if (value.TryGetValue("b", out _)) discs["b"] = value["b"];
+				if (value.TryGetValue("m", out _)) discs["m"] = value["m"];
+			}
+		}
 	}
 }
