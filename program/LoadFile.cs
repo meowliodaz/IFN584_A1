e8fe7539ec2c3a43bd19b88d2337dcaf1cd97e2f
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-10 23:28:24 +1000

    [load] Create new data type for loading old game

---
+++ program/LoadFile.cs
@@ -0,0 +1,11 @@
+namespace Connect4
+{
+	public class LoadFile
+	{
+		// Fields
+		public List<List<string>> grid;
+		public Dictionary<string, string> p1;
+		public Dictionary<string, string> p2;
+
+	}
+}
