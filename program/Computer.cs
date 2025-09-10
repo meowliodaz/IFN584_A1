360c7ec5de3c580677da663073e97ae89faa2df1
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-10 13:57:20 +1000

    [player] Computer, copy grid behaviours

--- program/Computer.cs
+++ program/Computer.cs
@@ -2,6 +2,19 @@ namespace Connect4
 {
 	public class Computer : Player
 	{
-		
-	}
+		private List<List<string>> GridCopy = new();
+		public Computer(int start_discs) : base(start_discs)
+		{
+			Util.LogString($"Computer: disc {Discs["o"]}");
+		}
+
+		public void setGridCopy(List<List<string>> grid_)
+		{
+			GridCopy = grid_;
+		}
+		public void logGridCopy()
+		{
+			Util.LogMatrix(GridCopy);
+		}
+    }
 }
