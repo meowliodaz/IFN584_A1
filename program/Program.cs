564dd5b767a05819b2f845871df626255487eb99
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-10 13:57:58 +1000

    [main] Test computer grid copy

--- program/Program.cs
+++ program/Program.cs
@@ -5,7 +5,13 @@
 		static void Main()
 		{
 			Util.DeleteLog("string.log");
-			Grid grid = new(7, 6);
+			Computer P1 = new(99);
+			Player P2 = new(1);
+
+			Grid grid = new(7, 6, P1, P2);
+
+
+
 			// Test case 1
 			string play1 = "o1,o1,o1,o2,m1,b1,o1,o3,o1,o4";
 			// Test case 2
@@ -14,13 +20,10 @@
 			string play3 = "o1,o2,o2,o3,o3,o4,o3,o4,o5,o4,o4";
 			// Test case 4
 			string play4 = "o2,o2,o2,o2,o3,o4,o3,o3,o1,o4,o1,o5";
-			// Test case 5
-			string play5 = "o1,o2,o1,o2,o1,o2,o1,o2";
-			// Test case 6
-			string play6 = "o1,o2,o1,o2,o1,o2,o1,o2";
-			// Test case 7
-			string play7 = "o1,o2,o1,o2,o1,o2,o1,o2";
-			grid.TestUpdateGrid(play4);
+			grid.TestUpdateGrid(play2);
+			
+			P1.setGridCopy(grid.Matrix);
+			P1.logGridCopy();
 		}
 	}
 }
