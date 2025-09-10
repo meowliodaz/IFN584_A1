4c1e1ad8d280a8f70524d30ee5c5c487541acfaf
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-10 14:17:15 +1000

    [player] Computer play random

--- program/Computer.cs
+++ program/Computer.cs
@@ -3,6 +3,8 @@ namespace Connect4
 	public class Computer : Player
 	{
 		private List<List<string>> GridCopy = new();
+		private int GridCols = 0;
+		private int GridRows = 0;
 		public Computer(int start_discs) : base(start_discs)
 		{
 			Util.LogString($"Computer: disc {Discs["o"]}");
@@ -11,10 +13,39 @@ namespace Connect4
 		public void setGridCopy(List<List<string>> grid_)
 		{
 			GridCopy = grid_;
+			GridCols = GridCopy.Count;
+			GridRows = GridCopy[0].Count;
 		}
 		public void logGridCopy()
 		{
 			Util.LogMatrix(GridCopy);
+		}
+		public string PlayToWin()
+		{
+			return PlayRandom();
+		}
+		public string PlayRandom()
+		/*
+		Check for remaining discs of each type => randomly choose 1 viable disc
+		Check for viable columns => Randomly choose 1 column for placement
+		*/
+		{
+			Random rnd = new();
+			string move = "";
+			List<string> discList = new();
+			foreach (KeyValuePair<string, int> disc in Discs)
+			{
+				if (disc.Value > 0) discList.Add(disc.Key);
+			}
+			move += discList[rnd.Next(0, discList.Count)];
+			List<int> colList = new();
+			for (int c = 0; c < GridCols; c++)
+			{
+				if (GridCopy[c].IndexOf(" ") != -1) colList.Add(c);
+			}
+			move += $"{colList[rnd.Next(0, colList.Count)]}";
+
+			return move;
 		}
     }
 }
