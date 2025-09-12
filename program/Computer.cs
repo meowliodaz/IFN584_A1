28c64c04832ff80fcb8f9b7e040adedf9640edd2
Trieu Hai Pham <trieuhaipham32621998@gmail.com>
2025-09-12 15:44:57 +1000

    [player] Computer: Fix grid and play to win duplicated move

--- program/Computer.cs
+++ program/Computer.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace Connect4
 {
 	public class Computer : Player
@@ -15,7 +17,10 @@ namespace Connect4
 		// Methods
 		public void setGridCopy(List<List<string>> grid_)
 		{
-			GridCopy = grid_;
+			for (int c = 0; c < grid_.Count; c++)
+			{
+				GridCopy.Add(grid_[c]);
+			}
 			GridCols = GridCopy.Count;
 			GridRows = GridCopy[0].Count;
 			WinCondition = (int)(GridCols * GridRows * 0.1);
@@ -40,14 +45,14 @@ namespace Connect4
 					
 					if (CheckWinComputer())
 					{
-						return $"{discDict["o"]}{c + 1}";
+						GridCopy[c][r] = " ";
+						return $"o{c + 1}";
 					}
 					GridCopy[c][r] = " ";
 					break;
 				}
 
 			}
-
 			return PlayRandom();
 		}
 		public string PlayRandom()
@@ -59,17 +64,26 @@ namespace Connect4
 			Random rnd = new();
 			string move = "";
 			List<string> discList = new();
-			foreach (KeyValuePair<string, int> disc in Discs)
+			for (int d = 0; d < Discs["o"]; d++)
 			{
-				if (disc.Value > 0) discList.Add(disc.Key);
+				discList.Add("o");
+			}
+			for (int d = 0; d < Discs["b"]; d++)
+			{
+				discList.Add("b");
+			}
+			for (int d = 0; d < Discs["m"]; d++)
+			{
+				discList.Add("m");
 			}
 			move += discList[rnd.Next(0, discList.Count)];
+
 			List<int> colList = new();
 			for (int c = 0; c < GridCols; c++)
 			{
-				if (GridCopy[c].IndexOf(" ") != -1) colList.Add(c);
+				if (GridCopy[c].IndexOf(" ") != -1) colList.Add(c+1);
 			}
-			move += $"{colList[rnd.Next(1, colList.Count+1)]}";
+			move += $"{colList[rnd.Next(0, colList.Count)]}";
 
 			return move;
 		}
@@ -89,11 +103,13 @@ namespace Connect4
 				{
 					for (int r = 0; r < GridRows; r++)
 					{
-						if (c + dCol < 0 | r + dRow < 0 | c + dCol > GridCols | r + dRow > GridRows) continue;
+						if (c + dCol < 0 | r + dRow < 0 | c + dCol >= GridCols | r + dRow >= GridRows) continue;
 						string rootCell = GridCopy[c][r];
 						for (int j = longestDistance - 1; j >= 0; j--)
 						{
-							string comparedCell = GridCopy[c + direction8[i, 0] * (longestDistance - j)][r + direction8[i, 1] * (longestDistance - j)];
+							int cellCol = c + direction8[i, 0] * (longestDistance - j);
+							int cellRow = r + direction8[i, 1] * (longestDistance - j);
+							string comparedCell = GridCopy[cellCol][cellRow];
 							if (rootCell == " " | rootCell != comparedCell)
 							{
 								WinCheckCount = 1;
